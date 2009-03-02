19a37e623992ddd4ca66ae645e2a56f1cc19da2a
pgeorges <pgeorges>
2009-03-02 18:54:16 +0000

    *** empty log message ***

--- tcl/start.tcl
+++ tcl/start.tcl
@@ -34,7 +34,7 @@ exec `dirname $0`/tkscid "$0" "$@"
 ############################################################
 
 # Alter the version if any patches have been made to the Tcl code only:
-set scidVersion "3.7"
+set scidVersion "3.7.1"
 set scidVersionDate "March 2009"
 
 package require Tcl 8.5
