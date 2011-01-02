4a19f6082508cc8b36ac6b552ca1c6e447721df7
arwagner <arwagner>
2011-01-02 14:10:26 +0000

    - Fix scidlet

--- src/engine.h
+++ src/engine.h
@@ -111,7 +111,9 @@ private:
     int      MinSearchTime; // Minimum search time in milliseconds.
     int      MaxSearchTime; // Maximum search time in milliseconds.
     uint     MinDepthCheckTime; // will not check time before this depth is reached
+    bool     Debug;         // If true, print debug info to stdout.
     bool     PostInfo;      // If true, print post info to stdout.
+    bool     XBoardMode;    // If true, print info in xboard format.
     bool     Pruning;       // If true, do futility pruning.
 #ifndef WINCE
     FILE *   LogFile;       // Output is to stdout and to this file.
@@ -131,13 +133,17 @@ private:
     principalVarT PV [ENGINE_MAX_PLY];   // Principal variation at each ply.
     simpleMoveT KillerMove [ENGINE_MAX_PLY][2];  // Two killer moves per ply.
     int History[16][64];    // Success history of piece-to-square moves.
-    byte     TranTableSequence;       // Transposition table sequence number.
-    static const uint TranTableSize = 2048;  // Number of Transposition table entries.
-    transTableEntryT TranTable[2048]; // Transposition table.
-    static const uint PawnTableSize = 64;    // Number of Pawn structure table entries.
-    pawnTableEntryT PawnTable[64];    // Pawn structure score hash table.
+    byte     TranTableSequence;    // Transposition table sequence number.
+    uint     TranTableSize;        // Number of Transposition table entries.
+    transTableEntryT * TranTable;  // Transposition table.
+    uint     PawnTableSize;        // Number of Pawn structure table entries.
+    pawnTableEntryT * PawnTable;   // Pawn structure score hash table.
+    bool (*CallbackFunction)(Engine *, void *);  // Periodic callback.
     void *   CallbackData;
+    simpleMoveT * GameMoves [1024];
+    uint      NumGameMoves;
 
+private:
     int PieceValue (pieceT piece);
     int SearchRoot (int depth, int alpha, int beta, MoveList * mlist);
     int Search (int depth, int alpha, int beta, bool tryNullMove);
@@ -172,30 +178,249 @@ private:
     void ScorePawnStructure (pawnTableEntryT * pawnEntry);
     bool IsMatingScore (int score);
     bool IsGettingMatedScore (int score);
-    uint RepeatedPosition (void);
 
     bool OutOfTime (void);
-    bool NoMatingMaterial (void);
-    bool FiftyMoveDraw (void);
+    void AdjustTime (bool easyMove);
 
 public:
+#ifdef WINCE
+  void* operator new(size_t sz) {
+    void* m = my_Tcl_Alloc(sz);
+    return m;
+  }
+  void operator delete(void* m) {
+    my_Tcl_Free((char*)m);
+  }
+  void* operator new [] (size_t sz) {
+    void* m = my_Tcl_AttemptAlloc(sz);
+    return m;
+  }
 
-	Engine();
-	void SetSearchDepth (uint ply);
-    void SetSearchTime (uint ms);
-    void SetSearchTime (uint min, uint ms, uint max);
+  void operator delete [] (void* m) {
+    my_Tcl_Free((char*)m);
+  }
+
+#endif
+    Engine()   {
+        MaxDepth = ENGINE_MAX_PLY;      // A large default search depth
+        SearchTime = 1000;  // Default search time: 1000 ms = one second.
+        MinSearchTime = MaxSearchTime = SearchTime;
+        MinDepthCheckTime = 4; // will not check time until depth is at least of this value
+#ifndef WINCE
+        LogFile = NULL;
+#endif
+        Debug = false;
+        PostInfo = false;
+        XBoardMode = false;
+        Pruning = false;
+        RepStackSize = 0;
+        TranTable = NULL;
+        TranTableSize = 0;
+        TranTableSequence = 0;
+        PawnTable = NULL;
+        PawnTableSize = 0;
+        SetHashTableKilobytes (ENGINE_HASH_KB);
+        SetPawnTableKilobytes (ENGINE_PAWN_KB);
+        CallbackFunction = NULL;
+        NumGameMoves = 0;
+        RootPos.StdStart();
+        Pos.StdStart();
+        PV[0].length = 0;
+    }
+#ifdef WINCE
+    ~Engine()  { my_Tcl_Free((char*) TranTable);  my_Tcl_Free((char*) PawnTable); }
+#else
+    ~Engine()  { delete[] TranTable;  delete[] PawnTable; }
+#endif
+    void SetSearchDepth (uint ply) {
+        if (ply < 1) { ply = 1; }
+        if (ply > ENGINE_MAX_PLY) { ply = ENGINE_MAX_PLY; }
+        MaxDepth = ply;
+    }
+    void SetSearchTime (uint ms) {
+        MinSearchTime = SearchTime = MaxSearchTime = ms;
+    }
+    void SetSearchTime (uint min, uint ms, uint max) {
+        MinSearchTime = min;
+        SearchTime = ms;
+        MaxSearchTime = max;
+    }
+    void SetMinDepthCheckTime(uint depth) {
+      MinDepthCheckTime = depth;
+    }
+    void SetDebug (bool b) { Debug = b; }
+    void SetPostMode (bool b) { PostInfo = b; }
+    bool InPostMode (void) { return PostInfo; }
+    void SetXBoardMode (bool b) { XBoardMode = b; }
+    bool InXBoardMode (void) { return XBoardMode; }
+    void SetPruning (bool b) { Pruning = b; }
 #ifndef WINCE
     void SetLogFile (FILE * fp) { LogFile = fp; }
 #endif
+    void SetHashTableKilobytes (uint sizeKB);
+    void SetPawnTableKilobytes (uint sizeKB);
+    uint NumHashTableEntries (void) { return TranTableSize; }
+    uint NumPawnTableEntries (void) { return PawnTableSize; }
     void ClearHashTable (void);
     void ClearPawnTable (void);
+    void ClearHashTables (void) {
+        ClearHashTable();
+        ClearPawnTable();
+    }
+
+    void SetCallbackFunction (bool (*fn)(Engine *, void *), void * data) {
+        CallbackFunction = fn;
+        CallbackData = data;
+    }
+
+    uint GetNodeCount (void) { return NodeCount; }
+
+    bool NoMatingMaterial (void);
+    bool FiftyMoveDraw (void);
+    uint RepeatedPosition (void);
+
     void SetPosition (Position * pos);
+    Position * GetPosition (void) { return &RootPos; }
+    void PlayMove (simpleMoveT * move);
+    void RetractMove (void);
     int Score (void);
     int ScoreMaterial (void);
+    principalVarT * GetPV (void) { return &(PV[0]); }
+    uint PerfTest (uint depth);
+    uint ElapsedTime (void) { return Elapsed.MilliSecs(); }
     int Think (MoveList * mlist);
-
-	uint PerfTest (uint depth);
 };
+
+
+inline void
+Engine::SetPVLength (void)
+{
+    if (Ply < ENGINE_MAX_PLY - 1)  {PV[Ply].length = Ply; }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Engine::UpdatePV
+//   Updates the principal variation at the current Ply to
+//   include the specified move.
+inline void
+Engine::UpdatePV (simpleMoveT * sm)
+{
+    if (Ply >= ENGINE_MAX_PLY - 1) { return; }
+    if (InNullMove > 0) { return; }
+    // if (! Pos.IsLegalMove (sm)) { return; }
+
+    PV[Ply].move[Ply] = *sm;
+    for (uint j = Ply + 1; j < PV[Ply + 1].length; j++) {
+        PV[Ply].move[j] = PV[Ply+1].move[j];
+    }
+    PV[Ply].length = PV[Ply+1].length;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Killer moves:
+//   We keep track of two "killer" moves at each ply, moves which
+//   are not captures or promotions (as they get ordered first) but
+//   were good enough to cause a beta cutoff. Killer moves get
+//   ordered after good captures but before non-killer noncaptures,
+//   which are ordered using the history table (see below).
+//
+//   Only noncaptures and non-promotion moves can be killer moves, but
+//   we make an exception for those that have a negative score (meaning
+//   they lose material according to the static exchange evaluator),
+//   since they would otherwise be searched last after all noncaptures.
+
+inline void
+Engine::ClearKillerMoves (void)
+{
+    for (uint i=0; i < ENGINE_MAX_PLY; i++) {
+        KillerMove[i][0].from = NULL_SQUARE;
+        KillerMove[i][1].from = NULL_SQUARE;
+    }
+}
+
+inline void
+Engine::AddKillerMove (simpleMoveT * sm)
+{
+    if (sm->capturedPiece != EMPTY  &&  sm->score >= 0) { return; }
+    if (sm->promote != EMPTY  &&  sm->score >= 0) { return; }
+    simpleMoveT * killer0 = &(KillerMove[Ply][0]);
+    simpleMoveT * killer1 = &(KillerMove[Ply][1]);
+    if (killer0->from == sm->from  &&  killer0->to == sm->to
+          &&  killer0->movingPiece == sm->movingPiece) {
+        return;
+    }
+    *killer1 = *killer0;
+    *killer0 = *sm;
+}
+
+inline bool
+Engine::IsKillerMove (simpleMoveT * sm)
+{
+    simpleMoveT * killer0 = &(KillerMove[Ply][0]);
+    if (killer0->from == sm->from  &&  killer0->to == sm->to
+          &&  killer0->movingPiece == sm->movingPiece) {
+        return true;        
+    }
+    simpleMoveT * killer1 = &(KillerMove[Ply][1]);
+    if (killer1->from == sm->from  &&  killer1->to == sm->to
+          &&  killer1->movingPiece == sm->movingPiece) {
+        return true;        
+    }
+    return false;
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// History table:
+//   This is a table of values indexed by moving piece and
+//   target square, indicating the historical success of each move
+//   as measured by the frequency of "good" (better than alpha)
+//   scores. It is used to order non-capture moves after killers.
+
+inline void
+Engine::ClearHistoryValues (void)
+{
+    for (pieceT p = WK; p <= BP; p++) {
+        for (squareT to = A1; to <= H8; to++) {
+            History[p][to] = 0;
+        }
+    }
+}
+
+inline void
+Engine::HalveHistoryValues (void)
+{
+    // Output("# Halving history values\n");
+    for (pieceT p = WK; p <= BP; p++) {
+        for (squareT to = A1; to <= H8; to++) {
+            History[p][to] /= 2;
+        }
+    }
+}
+
+inline void
+Engine::IncHistoryValue (simpleMoveT * sm, int increment)
+{
+    if (sm->capturedPiece != EMPTY  &&  sm->score >= 0) { return; }
+    if (sm->promote != EMPTY  &&  sm->score >= 0) { return; }
+    pieceT p = sm->movingPiece;
+    squareT to = sm->to;
+    ASSERT (p <= BP  &&  to <= H8);
+    History[p][to] += increment;
+    // Halve all history values if this one gets too large, to avoid
+    // non-capture moves getting searched before captures:
+    if (History[p][to] >= ENGINE_MAX_HISTORY) {
+        HalveHistoryValues();
+    }
+}
+
+inline int
+Engine::GetHistoryValue (simpleMoveT * sm)
+{
+    pieceT p = sm->movingPiece;
+    squareT to = sm->to;
+    ASSERT (p <= BP  &&  to <= H8);
+    return History[p][to];
+}
 
 #endif  // SCID_ENGINE_H
 
