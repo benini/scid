ffe38b140cebfdd3c976541104212fc05fe6026b
Fulvio <fbf@libero.it>
2012-01-27 00:37:03 +0100

    Added gpl copyleft statement to sortcache.h and sortcache.cpp (This should be added to every file, but it's problematic to know who contributed to older files. At least starting to add the gpl statement to new files)

--- src/sortcache.h
+++ src/sortcache.h
@@ -1,3 +1,21 @@
+/*
+* Copyright (C) 2011  Gerd Lorscheid, Fulvio Benini
+
+* This file is part of Scid (Shane's Chess Information Database).
+*
+* Scid is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation.
+*
+* Scid is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Scid.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef SORTINGCACHE_DEFINED
 #define SORTINGCACHE_DEFINED
 
