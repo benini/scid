ab47d6994ee6e0bbc2312080cebda09d4899ce81
Mike Curtis <Michael.C.Curtis@gmail.com>
2013-04-08 19:47:13 -0500

    Fixed popup board for subvariations

--- tcl/windows/pgn.tcl
+++ tcl/windows/pgn.tcl
@@ -176,6 +176,8 @@ namespace eval pgn {
       if {[winfo exists .pgnWin.text.ctxtMenu]} { destroy .pgnWin.text.ctxtMenu; focus .pgnWin }
     }
     
+    bind $w <ButtonRelease-$::MB2> ::pgn::HideBoard
+
     # Bind right button to popup a contextual menu:
     bind $w <ButtonPress-$::MB3> "::pgn::contextMenu .pgnWin.text 5 %x %y %X %Y"
     
@@ -322,8 +324,11 @@ namespace eval pgn {
   #
   ################################################################################
   proc ShowBoard {win moveTag xc yc} {
-    set numMoves [expr {[string range $moveTag 2 end] - 1}]
-    set bd [sc_pos pgnBoard [sc_game firstMoves 0 $numMoves]]
+    set offSet [sc_pos pgnOffset]
+    sc_move pgn [string range $moveTag 2 end]
+    set bd [sc_pos board]
+    sc_move pgn $offSet
+
     set w .pgnPopup
     set psize 30
     if {$psize > $::boardSize} { set psize $::boardSize }
