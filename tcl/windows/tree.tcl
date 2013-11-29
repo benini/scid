4815708adb182dd4751cc5b9d3bdcf4b1c420800
Fulvio <fbf@libero.it>
2013-11-29 16:38:30 +0100

    Bug fix: closing a base with a tree window on it cause "Invalid database number" Thanks to Michael G. Thomas

--- tcl/windows/tree.tcl
+++ tcl/windows/tree.tcl
@@ -278,7 +278,6 @@ proc ::tree::closeTree {baseNumber} {
   destroy .treeWin$baseNumber
   sc_tree clean $baseNumber
   if {$::tree(locked$baseNumber)} { ::file::Close $baseNumber }
-  ::windows::stats::Refresh;
   set curr_base [sc_base current]
   set ::treeWin [winfo exists .treeWin$curr_base]
 }
