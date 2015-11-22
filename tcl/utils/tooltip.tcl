8ef08c1cb185e61ae8479bb91930ff04e81ed86b
Jens Hoffmann <xmcpam@gmail.com>
2015-11-22 19:55:31 +0100

    Use tklib tooltip implementation

--- tcl/utils/tooltip.tcl
+++ tcl/utils/tooltip.tcl
@@ -1,172 +1,27 @@
 ###
 #
-# ToolTips
+# Tooltips
 #
 
-namespace eval ::utils::tooltip {}
+package require tooltip
 
-set ::utils::tooltip::showToolTips 1
-set ::utils::tooltip::time 0
-set ::utils::tooltip::enteredWidget {}
-set ::utils::tooltip::tooltipDelay 400
-set ::utils::tooltip::tooltipWidth 30
+namespace eval ::utils::tooltip {
+  variable font
 
-array set ::utils::tooltip::message {}
-
-# Construct tooltip window:
-#
-toplevel .tooltip
-label .tooltip.text -relief solid -borderwidth 1 -justify left \
-    -background #f5f5f5 -padx 3 -pady 1 -font font_Tiny
-pack .tooltip.text -side left
-wm overrideredirect .tooltip 1
-if { $macOS }  {
-  ::tk::unsupported::MacWindowStyle style .tooltip help none
-}
-bind .tooltip <Button-1> { wm withdraw .tooltip }
-wm withdraw .tooltip
-
-
-# ::utils::tooltip::Set
-#
-#   Set the tooltip message for <button> to be <msg>
-#
-proc ::utils::tooltip::Set { button msg } {
-  variable message
-  set msg [string trim $msg]
-  if {$msg == ""} { return }
-  regsub {\\n} $msg "\n" msg
-  set msg [::utils::tooltip::trimWidth $msg]
-  set message($button) $msg
-  bind $button <Any-Enter> +[list ::utils::tooltip::Enter $button]
-  bind $button <Any-Leave> +[list ::utils::tooltip::Leave $button]
-}
-
-# ::utils::tooltip::SetTag
-#
-#   remove a tooltip message for a given button
-#
-proc ::utils::tooltip::UnSet { button } {
-  variable message
-  if { [info exists ::utils::tooltip::message($button)] } {
-	  unset message($button)
-	  bind $button <Any-Enter> ""
-	  bind $button <Any-Leave> ""
+  if {![info exists font]} {
+    set font TkDefaultFont
   }
-}
 
-# ::utils::tooltip::SetTag
-#
-#   Set the tooltip message for a text with tag <tag> to be <msg>
-#
-proc ::utils::tooltip::SetTag { text_widget msg tag } {
-  variable message
-  set msg [string trim $msg]
-  if {$msg == ""} { return }
-  regsub {\\n} $msg "\n" msg
-  set msg [::utils::tooltip::trimWidth $msg]
-  set message(${tag}_$text_widget) $msg
-  $text_widget tag bind $tag <Any-Enter> +[list ::utils::tooltip::Enter ${tag}_$text_widget]
-  $text_widget tag bind $tag <Any-Leave> +[list ::utils::tooltip::Leave ${tag}_$text_widget]
-}
+  # Undocumented tooltip variable that allows users to extend / override
+  # label creation options.
+  set tooltip::labelOpts [list -highlightthickness 0 -relief solid -bd 1 \
+      -background lightyellow -fg black -font $font]
 
-# ::utils::tooltip::Enter
-#
-#   Handles the mouse entering a button which has a tooltip.
-#
-proc ::utils::tooltip::Enter {button} {
-  variable showToolTips
-  variable enteredWidget
-  variable tooltipDelay
-  
-  if {! $showToolTips} { return }
-  set enteredWidget $button
-  after $tooltipDelay [list ::utils::tooltip::Check $button]
-}
-
-
-# ::utils::tooltip::Check
-#
-#   Called a set time after the mouse has entered a button with a
-#   tooltip, to check if it is still there. If so, the tooltip
-#   message is displayed.
-#
-proc ::utils::tooltip::Check {button} {
-  variable enteredWidget
-  
-  if {$enteredWidget != $button} {
-    # The mouse cursor has moved somewhere else; display no tooltip
-    return
+  proc Set {button msg} {
+    tooltip::tooltip $button $msg
   }
-  
-  if {! [info exists ::utils::tooltip::message($button)]} { return }
-  
-  .tooltip.text configure -text [tr $::utils::tooltip::message($button)]
-  set x [winfo pointerx .]
-  set y [winfo pointery .]
-  incr x 10
-  incr y 4
-  catch {wm transient .tooltip [winfo toplevel $button]}
-  
-  # make the tooltip visible
-  set maxw [ winfo vrootwidth .]
-  set maxh [ winfo vrootheight .]
-  set w [winfo reqwidth .tooltip]
-  set h [winfo reqheight .tooltip]
-  if { [expr $x + $w] > $maxw } {
-    set x [expr $maxw - $w]
-  }
-  if { [expr $y + $h] > $maxh } {
-    set y [expr $maxh - $h]
-  }
-  
-  catch {wm geometry .tooltip +$x+$y}
-  wm deiconify .tooltip
-  raise .tooltip
-  
-  # Automatically erase tooltip after a short delay
-  after 15000 [ list ::utils::tooltip::Leave $button ]
-}
 
-
-# ::utils::tooltip::Leave
-#
-#   Handles the mouse leaving a button which has a tooltip.
-#
-proc ::utils::tooltip::Leave {button} {
-  after cancel [ list ::utils::tooltip::Leave $button ]
-  variable showToolTips
-  if {! $showToolTips} { return }
-  wm withdraw .tooltip
-  after cancel [list ::utils::tooltip::Check $button]
-}
-
-# ::utils::tooltip::ToWidth
-#
-#   Returns the arg with CR every tooltipWidth chars at most
-#
-proc ::utils::tooltip::trimWidth { msg } {
-  set ret ""
-  foreach line [split $msg "\n"] {
-    
-    if {[string length $line] < $::utils::tooltip::tooltipWidth} {
-      append ret "$line\n"
-      continue
-    } else  {
-      # must split the line
-      set words [split $line " "]
-      
-      while {[llength $words] > 0} {
-        set tmp [lindex $words 0]
-        set words [lreplace $words 0 0]
-        while {[string length "$tmp [lindex $words 0]"] < $::utils::tooltip::tooltipWidth && [llength $words] > 0} {
-          append tmp " " [lindex $words 0]
-          set words [lreplace $words 0 0]
-        }
-        append ret $tmp "\n"
-      }
-      
-    }
+  proc SetTag {text_widget msg tag} {
+    tooltip::tooltip $text_widget -tag $tag $msg
   }
-  return [string trim $ret]
 }
