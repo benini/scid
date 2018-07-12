933ae0070828472d721afd20fb20ca39b89f7be6
Fulvio <fbf@libero.it>
2018-07-12 10:40:06 +0200

    Re-implement BoardSearch dialog

--- tcl/search/board.tcl
+++ tcl/search/board.tcl
@@ -1,91 +1,200 @@
-###
-### search/board.tcl: Board Search routines for Scid.
-###
+# Copyright (C) 2018 Fulvio Benini
+#
+# This file is part of Scid (Shane's Chess Information Database).
+#
+# Scid is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation.
+#
+# Scid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scid.  If not, see <http://www.gnu.org/licenses/>.
 
-set searchInVars 0
-set sBoardIgnoreCols 0
-set sBoardSearchType Exact
+namespace eval ::search {}
+
+# Create a search window using a common framework.
+# It manages the database, the filter and the and/or/reset combination of the
+# results with the previous filter's values.
+# @param create_subwnd: a function that creates all the specific widgets of a
+#                       search window. Should return a valid command that when
+#                       evaluated returns a list of the search's options.
+proc ::search::Open {ref_base ref_filter title create_subwnd} {
+	set w ".wnd_$title"
+	::win::createDialog $w
+	::setTitle $w [::tr $title]
+
+	grid [ttk::frame $w.refdb] -sticky news
+	CreateSelectDBWidget "$w.refdb" "::search::dbase_($w)" "$ref_base"
+	set ::search::filter_($w) $ref_filter
+
+	grid [ttk::frame $w.options] -sticky news
+	set options_cmd [$create_subwnd $w.options]
+
+	grid [ttk::labelframe $w.filterOp] -sticky news -pady 8
+	if {![info exists ::search::filterOp_($w)]} {
+		set ::search::filterOp_($w) reset
+	}
+	ttk::radiobutton $w.filterOp.and   -text [::tr FilterAnd]    -variable ::search::filterOp_($w) -value and
+	ttk::radiobutton $w.filterOp.or    -text [::tr FilterOr]     -variable ::search::filterOp_($w) -value or
+	ttk::radiobutton $w.filterOp.reset -text [::tr FilterIgnore] -variable ::search::filterOp_($w) -value reset
+	grid $w.filterOp.and $w.filterOp.or $w.filterOp.reset -ipadx 8
+
+	grid [ttk::frame $w.buttons] -sticky news
+	ttk::button $w.buttons.save -text [::tr Save] -command "::search::save_ $options_cmd"
+	ttk::button $w.buttons.search_new -text "[tr Search] ([tr GlistNewSort] [tr Filter])" \
+		-command "::search::start_newflt_ $w $options_cmd"
+	ttk::button $w.buttons.search -text [::tr Search] -command "::search::start_ $w $options_cmd"
+	grid $w.buttons.save x $w.buttons.search_new $w.buttons.search -sticky w -padx "0 5"
+	grid columnconfigure $w.buttons 1 -weight 1
+
+	ttk::button $w.buttons.stop -text [::tr Stop] -command progressBarCancel
+	canvas $w.progressbar -width 300 -height 20 -bg white -relief solid -border 1 -highlightthickness 0
+	$w.progressbar create rectangle 0 0 0 0 -fill blue -outline blue -tags bar
+	$w.progressbar create text 295 10 -anchor e -font font_Regular -tags time
+	grid $w.buttons.stop -row 0 -column 0
+	grid $w.progressbar -in $w.buttons -row 0 -column 1 -columnspan 3 -sticky w
+	progressbar_ $w hide
+
+	::search::Refresh $w
+}
+
+proc ::search::Refresh {w} {
+	# TODO:
+	# Update CreateSelectDBWidget
+	# set filter to "dbfilter" if the filter no longer exists because the gamelist was closed
+
+	# TODO:
+	$w.buttons.save configure -state disabled
+
+	# TODO:
+	$w.buttons.search_new configure -state disabled
+
+	lassign [sc_filter sizes $::search::dbase_($w) $::search::filter_($w)] filterSz gameSz
+	set n_games [::windows::gamelist::formatFilterText $filterSz $gameSz]
+	$w.filterOp configure -text "[::tr FilterOperation] ($n_games)"
+}
+
+proc ::search::progressbar_ {w show_hide} {
+	if {$show_hide eq "show"} {
+		grid remove $w.buttons.save
+		grid remove $w.buttons.search_new
+		grid remove $w.buttons.search
+		grid $w.progressbar
+		progressBarSet $w.progressbar 301 21
+		grid $w.buttons.stop
+		grab $w.buttons.stop
+	} else {
+		grab release $w.buttons.stop
+		grid remove $w.buttons.stop
+		grid remove $w.progressbar
+		grid $w.buttons.save
+		grid $w.buttons.search_new
+		grid $w.buttons.search
+	}
+}
+
+proc ::search::save_ {options_cmd} {
+	# TODO:
+}
+
+proc ::search::start_ {w options_cmd} {
+	::search::progressbar_ $w show
+	::search::do_search_ $::search::dbase_($w) $::search::filter_($w) $::search::filterOp_($w) [$options_cmd]
+	::search::progressbar_ $w hide
+
+	::notify::DatabaseModified $::search::dbase_($w) $::search::filter_($w)
+	::search::Refresh $w
+}
+
+proc ::search::start_newflt_ {w options_cmd} {
+	set dbase $::search::dbase_($w)
+	set filter [sc_filter new $dbase]
+	sc_filter copy $dbase $filter $::search::filter_($w)
+
+	::search::progressbar_ $w show
+	::search::do_search_ $dbase $filter $::search::filterOp_($w) [$options_cmd]
+	::search::progressbar_ $w hide
+
+	# TODO:
+	# if interrupted
+	#     delete filter
+	#     return
+
+	set ::search::filter_($w) $filter
+	::windows::gamelist::Open $dbase $filter
+	::search::Refresh $w
+}
+
+proc ::search::do_search_ {dbase filter filter_op options} {
+	switch $filter_op {
+		reset {
+			sc_filter reset $dbase $filter full
+		}
+		and {
+		}
+		or {
+			set or_filter [sc_filter new $dbase]
+			sc_filter copy $dbase $or_filter $filter
+			sc_filter negate $dbase $filter
+		}
+	}
+
+	# TODO: sc_filter search $dbase $filter {*}$options
+	sc_search board 0 {*}$options $dbase
+
+	if {[info exists or_filter]} {
+		sc_filter or $dbase $filter $or_filter
+		sc_filter release $dbase $or_filter
+	}
+}
+
 
 # ::search::board
 #   Opens the search window for the current board position.
 #
 proc ::search::board {{ref_base ""}} {
-  global searchInVars sBoardType sBoardIgnoreCols
-  
-  set w .sb
-  if {[winfo exists $w]} {
-    wm deiconify $w
-    raiseWin $w
-    return
-  }
-  
-  toplevel $w
-  wm title $w "Scid: $::tr(BoardSearch)"
-  
-  bind $w <Escape> "$w.b.cancel invoke"
-  bind $w <Return> "$w.b.search invoke"
-  bind $w <F1> { helpWindow Searches Board }
+	::search::Open $ref_base dbfilter BoardSearch ::search::boardCreateFrame
+}
 
-  pack [ttk::frame $w.refdb] -side top -fill x
-  CreateSelectDBWidget "$w.refdb" "refDatabaseB" "$ref_base"
-  addHorizontalRule $w
+proc ::search::boardCreateFrame {w} {
+	ttk::labelframe $w.pos -text [::tr SearchType]
+	grid $w.pos -sticky news -pady 6
+	grid columnconfigure $w 0 -weight 1
+	if {![info exists ::search::boardOptType_]} {
+		set ::search::boardOptType_ Exact
+	}
+	ttk::radiobutton $w.pos.exact -textvar ::tr(SearchBoardExact)  -variable ::search::boardOptType_ -value Exact
+	ttk::radiobutton $w.pos.pawns -textvar ::tr(SearchBoardPawns)  -variable ::search::boardOptType_ -value Pawns
+	ttk::radiobutton $w.pos.files -textvar ::tr(SearchBoardFiles)  -variable ::search::boardOptType_ -value Fyles
+	ttk::radiobutton $w.pos.material -textvar ::tr(SearchBoardAny) -variable ::search::boardOptType_ -value Material
+	grid $w.pos.exact -sticky w
+	grid $w.pos.pawns -sticky w
+	grid $w.pos.files -sticky w
+	grid $w.pos.material -sticky w
 
-  ttk::label $w.type -textvar ::tr(SearchType) -font font_Bold -anchor center
-  pack $w.type -side top -expand 1 -fill x
-  pack [ttk::frame $w.g] -side top -fill x
-  ttk::radiobutton $w.g.exact -textvar ::tr(SearchBoardExact) -variable sBoardSearchType -value Exact
-  ttk::radiobutton $w.g.pawns -textvar ::tr(SearchBoardPawns) -variable sBoardSearchType -value Pawns
-  ttk::radiobutton $w.g.files -textvar ::tr(SearchBoardFiles) -variable sBoardSearchType -value Fyles
-  ttk::radiobutton $w.g.material -textvar ::tr(SearchBoardAny) -variable sBoardSearchType -value Material
-  set row 0
-  foreach i {exact pawns files material} {
-    grid $w.g.$i -row $row -column 0 -sticky w
-    incr row
-  }
-  addHorizontalRule $w
-  
-  ::search::addFilterOpFrame $w
-  addHorizontalRule $w
-  
-  ### Progress bar:
-  
-  ttk::frame $w.fprogress
-  canvas $w.fprogress.progress -height 20 -width 300 -bg white -relief solid -border 1
-  $w.fprogress.progress create rectangle 0 0 0 0 -fill blue -outline blue -tags bar
-  $w.fprogress.progress create text 295 10 -anchor e -font font_Regular -tags time -fill black -text "0:00 / 0:00"
-  pack $w.fprogress.progress -side top -anchor center -expand 1 -pady 2
-  
-  ttk::frame $w.b2
-  pack $w.b2 -side top -fill x
-  ttk::frame $w.b
-  pack $w.b -side top -fill x
-  ttk::checkbutton $w.b2.vars -textvar ::tr(LookInVars) -onvalue 1 -offvalue 0 -variable searchInVars
-  ttk::checkbutton $w.b2.flip -textvar ::tr(IgnoreColors) -onvalue 1 -offvalue 0 -variable sBoardIgnoreCols
-  
-  dialogbutton $w.b.stop -textvar ::tr(Stop) -command progressBarCancel
-  $w.b.stop configure -state disabled
-  
-  dialogbutton $w.b.search -textvar ::tr(Search) -command {
-    .sb.b.stop configure -state normal
-    grab .sb.b.stop
+	if {![info exists ::search::boardOptInVars_]} {
+		set ::search::boardOptInVars_ 0
+	}
+	ttk::checkbutton $w.vars -text [::tr LookInVars] -variable ::search::boardOptInVars_ -onvalue 1 -offvalue 0
+	grid $w.vars -sticky w
 
-    regexp {(\d+).*} $::refDatabaseB -> base
-    progressBarSet .sb.fprogress.progress 301 21
-    set str [sc_search board $::search::filter::operation $sBoardSearchType $searchInVars $sBoardIgnoreCols $base]
+	if {![info exists ::search::boardOptIgnoreCol_]} {
+		set ::search::boardOptIgnoreCol_ 0
+	}
+	ttk::checkbutton $w.flip -text [::tr IgnoreColors] -variable ::search::boardOptIgnoreCol_ -onvalue 1 -offvalue 0
+	grid $w.flip -sticky w
 
-    grab release .sb.b.stop
-    .sb.b.stop configure -state disabled
-    #tk_messageBox -type ok -title $::tr(SearchResults) -message $str
-    .sb.status configure -text $str
-    ::notify::DatabaseModified $base dbfilter
-  }
-  dialogbutton $w.b.cancel -textvar ::tr(Close) -command "focus .; destroy $w"
-  pack $w.b2.vars $w.b2.flip -side left -pady 2 -padx 5
-  packbuttons right $w.b.cancel .sb.b.search .sb.b.stop
-  pack $w.fprogress -side top -fill x
-  ttk::label $w.status -text "" -width 1 -font font_Small -relief sunken -anchor w
-  pack $w.status -side bottom -fill x
-  wm resizable $w 0 0
-  ::search::Config
-  focus $w.b.search
+	return "::search::boardGetOptions"
+}
+
+proc ::search::boardGetOptions {} {
+	set options {}
+	lappend options $::search::boardOptType_
+	lappend options $::search::boardOptInVars_
+	lappend options $::search::boardOptIgnoreCol_
+	return $options
 }
