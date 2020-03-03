e5d54bd459bada2877f9ea6ecd149c84dd0cd9e0
Fulvio <fbf@libero.it>
2020-03-03 20:41:36 +0100

    test_encodeFEN

--- gtest/test_game.cpp
+++ gtest/test_game.cpp
@@ -217,3 +217,24 @@ TEST(Test_Game, empty_tag_name) {
 	});
 	EXPECT_EQ(i, 2);
 }
+
+TEST(Test_Game, encodeFEN) {
+	std::vector<unsigned char> encodedGame;
+	const char* kiwipete =
+	    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+	{
+		Game game;
+		game.SetStartFen(kiwipete);
+		IndexEntry ie;
+		game.Encode(encodedGame, ie);
+	}
+	{
+		ByteBuffer bbuf(encodedGame.data(), encodedGame.size());
+		Game game;
+		game.Decode(bbuf);
+		game.MoveToStart();
+		char str[1024];
+		game.currentPos()->PrintFEN(str, FEN_ALL_FIELDS);
+		EXPECT_STREQ(kiwipete, str);
+	}
+}
