99d363f512264fd1cd1717d02c4ad5460d2facfa
Fulvio <fbf@libero.it>
2021-05-10 09:07:36 +0200

    Simplify class SquareSet

--- src/sqmove.h
+++ src/sqmove.h
@@ -58,60 +58,17 @@ public:
 };
 
 class SquareSet {
-	uint Bits_a1h4;
-	uint Bits_a5h8;
+	unsigned long long bits_ = 0;
 
 public:
-	SquareSet() { Bits_a1h4 = Bits_a5h8 = 0; }
-	SquareSet(squareT* squares) {
-		Bits_a1h4 = Bits_a5h8 = 0;
-		AddAll(squares);
-	}
-
-	void Clear(void) { Bits_a1h4 = Bits_a5h8 = 0; }
-	void AddAll(void) { Bits_a1h4 = Bits_a5h8 = 0xFFFFFFFFu; }
-
 	void Add(squareT sq) {
-		ASSERT(sq <= H8);
-		if (sq <= H4) {
-			Bits_a1h4 |= (1 << sq);
-		} else {
-			Bits_a5h8 |= (1 << (sq & 31));
-		}
-	}
-
-	void AddAll(squareT* squares) {
-		while (true) {
-			squareT sq = *squares;
-			if (sq == NULL_SQUARE) {
-				break;
-			}
-			ASSERT(sq <= H8);
-			squares++;
-			if (sq <= H4) {
-				Bits_a1h4 |= (1 << sq);
-			} else {
-				Bits_a5h8 |= (1 << (sq & 31));
-			}
-		}
+		ASSERT(sq < 64);
+		bits_ |= 1ull << sq;
 	}
 
 	bool Contains(squareT sq) {
-		ASSERT(sq <= H8);
-		if (sq <= H4) {
-			return (Bits_a1h4 & (1 << sq)) != 0;
-		} else {
-			return (Bits_a5h8 & (1 << (sq & 31))) != 0;
-		}
-	}
-
-	void Remove(squareT sq) {
-		ASSERT(sq <= H8);
-		if (sq <= H4) {
-			Bits_a1h4 &= ~(1 << sq);
-		} else {
-			Bits_a5h8 &= ~(1 << (sq & 31));
-		}
+		ASSERT(sq < 64);
+		return (bits_ & (1ull << sq)) != 0;
 	}
 };
 
