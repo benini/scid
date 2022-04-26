ad16674b1fb5db05b87c2dbe7b1a2fa625078fdf
Fulvio <fbf@libero.it>
2022-04-26 20:24:29 +0200

    Move toggle FullScreen to the Options menu.

--- tcl/board.tcl
+++ tcl/board.tcl
@@ -681,11 +681,9 @@ proc ::board::newToolBar_ {{w} {varname}} {
   $m add command -label "  [tr ShowHideMaterial]" -image tb_BD_Material -compound left
   #TODO translate
   $m add command -label "  Show/Hide Score Bar" -image tb_BD_Scorebar -compound left
-  $m add command -label "  [tr FullScreen]" -image tb_BD_Fullscreen -compound left
   set ${varname}(tb_BD_Flip) "::board::flip $w"
   set ${varname}(tb_BD_Material) "::board::toggleMaterial $w"
   set ${varname}(tb_BD_Scorebar) "::board::toggleScorebar $w"
-  set ${varname}(tb_BD_Fullscreen) { wm attributes . -fullscreen [expr ![wm attributes . -fullscreen] ] }
 
   return $m
 }
