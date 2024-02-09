a7afa43de870923abadceb17092868e00e345c2e
Uwe <Uwe>
2024-02-09 23:27:18 +0100

    bugfix splitname
    leading blank removed

--- tcl/tools/pinfo.tcl
+++ tcl/tools/pinfo.tcl
@@ -204,6 +204,8 @@ proc ::pinfo::splitName { playerName } {
   set count [string first "," $playerName ]
   if { $count < 0 } {
     set count [string first " " $playerName ]
+  } elseif { [string index $playerName [expr $count + 1]] eq " " } {
+      incr countlen
   }
   if { $count > 0 } {
     set fname [string range $playerName [expr $count + $countlen] end]
